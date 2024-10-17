9d17540e417c9e758279d2af02edee3eee870562
Clad7987 <vicenters.10@gmail.com>
2024-10-17 09:29:56 -0300

    Update fapdungeon.py

--- scrapers/fapdungeon.py
+++ scrapers/fapdungeon.py
@@ -14,7 +14,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.FileHandler("logs/fapdungeon_scraping.log"),
+        logging.FileHandler("../logs/fapdungeon_scraping.log"),
     ],
 )
 
